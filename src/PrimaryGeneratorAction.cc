999214ca9c6ac002c6eb408b48dde0840716b4ac
fiorotto8 <davide.fiorina@gssi.it>
2025-10-30 19:06:47 +0100

    Minor Changes

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -9,6 +9,9 @@
 #include "G4SystemOfUnits.hh"
 #include "Randomize.hh"     // G4UniformRand, G4RandGauss
 
+#include "G4Exception.hh"   // for fatal abort if file missing
+#include "G4ExceptionSeverity.hh"
+
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -50,8 +53,16 @@ void PrimaryGeneratorAction::LoadSpectrum(const G4String& filename)
 {
   std::ifstream fin(filename);
   if (!fin.is_open()) {
-    G4cerr << "[PrimaryGeneratorAction] ERROR: cannot open " << filename << G4endl;
-    return;
+    // Print and abort the job
+    G4String msg = "Cannot open spectrum file: " + filename +
+                   "\n- Check the path (it is relative to the run directory)"
+                   "\n- Check permissions"
+                   "\n- Check that the file name is correct";
+    G4Exception("PrimaryGeneratorAction::LoadSpectrum",
+                "B3_SPECTRUM_FILE_NOT_FOUND",
+                FatalException,
+                msg);
+    return; // not reached, but silences some compilers
   }
 
   fBins.clear();
@@ -66,6 +77,7 @@ void PrimaryGeneratorAction::LoadSpectrum(const G4String& filename)
     int binid;
     G4double low, center, high, counts, err, pol, polErr;
     if (!(iss >> binid >> low >> center >> high >> counts >> err >> pol >> polErr)) {
+      // malformed line → skip
       continue;
     }
 
@@ -89,8 +101,10 @@ void PrimaryGeneratorAction::LoadSpectrum(const G4String& filename)
   fTotalW = cum;
 
   if (fTotalW <= 0.0) {
+    // spectrum exists but has no counts → this is a configuration problem,
+    // but here we just warn and let the run continue.
     G4cerr << "[PrimaryGeneratorAction] WARNING: spectrum " << filename
-          << " has zero total counts; default energy will be used.\n";
+           << " has zero total counts; default energy will be used.\n";
   }
 }
 
@@ -136,7 +150,7 @@ G4double PrimaryGeneratorAction::SampleEnergyFromSpectrum(G4double& outPolMean,
 //   else → (0,0,0)
 // --------------------------------------------------
 G4ThreeVector PrimaryGeneratorAction::SamplePolarizationVector(G4double polMean,
-                                                              G4double polSigma) const
+                                                               G4double polSigma) const
 {
   // draw p from Gaussian
   G4double p = polMean;
